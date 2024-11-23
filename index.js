9de4294148bb3637838ff6b80f8b764c943ad42b
kunalpawar83 <kunalpawar8319@gmail.com>
2024-11-23 16:47:49 +0530

    vercel done

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("*", (req, res) => {
-  res.status(200).json({ message: "vrecel Done" });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
