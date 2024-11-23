ce6fb5ff080f46d17afbfb54de75fb35c717b1f9
kunalpawar83 <kunalpawar8319@gmail.com>
2024-11-23 17:11:59 +0530

    school done

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -4,6 +4,6 @@ const SchoolController = require("../controllers/schoolControllers.js");
 
 // Define the route with the callback function
 router.post("/add-school", SchoolController.addSchool);
-router.get("/add-school", SchoolController.listSchools);
+router.get("/get-shools", SchoolController.listSchools);
 
 module.exports = router;
